11b71d39a67e451cdcd1a5170caa292af60efccc
ramonespier <ramon.coelhomelo@outlook.com>
2025-09-17 19:25:13 -0300

    style: blocks (precisa estilizar)

--- frontend/whisper/src/app/layout.jsx
+++ frontend/whisper/src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/Themes/theme-provider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -18,12 +19,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
+  </>
   );
 }
