c0eb2c25732e217deaeeccfe917f620952dad4f8
ramonespier <ramon.coelhomelo@outlook.com>
2025-09-23 17:05:13 -0300

    middleware de upload e model de livro

---
+++ backend/src/middlewares/upload.js
@@ -0,0 +1,30 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Tipo de arquivo não suportado! Apenas imagens são permitidas.'), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 5 // Limite de 5 MB
+  }
+});
+
+export default upload;
---
+++ backend/src/models/Book.js
@@ -0,0 +1,46 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../database/sequelize.js";
+
+class Book extends Model {}
+
+Book.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    title: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+
+    description: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+
+    image: {
+        type: DataTypes.STRING(1000),
+    },
+
+    genre: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+
+    author: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+
+    storage: {
+        type: DataTypes.INTEGER
+    }
+}, {
+    sequelize,
+    modelName: 'Book',
+    tableName: 'books',
+})
+
+export default Book
