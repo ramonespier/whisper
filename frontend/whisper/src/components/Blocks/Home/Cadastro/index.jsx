8106ff9f22451b14434a1fc578f702132b7cb110
ramonespier <ramon.coelhomelo@outlook.com>
2025-09-26 09:53:46 -0300

    theme button styles

--- frontend/whisper/src/components/Blocks/Home/Cadastro/index.jsx
+++ frontend/whisper/src/components/Blocks/Home/Cadastro/index.jsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { handleRegister } from "@/api/api";
+import { AdmTheme } from "@/components/Admin/AdmTheme";
+
 
 const FormCadastro = ({
   heading = "Cadastro",
@@ -17,6 +19,14 @@ const FormCadastro = ({
 }) => {
   return (
     <section className="bg-muted h-screen">
+
+      <Button asChild className="p-0 size-15 rounded-full fixed left-5 top-5 ">
+        <div>
+          <AdmTheme />
+        </div>
+      </Button>
+
+
       <div className="flex h-full items-center justify-center">
         <div className="flex flex-col items-center gap-6 lg:justify-start">
           {/* Logo */}
@@ -31,53 +41,53 @@ const FormCadastro = ({
 
           <form action={handleRegister}>
             <div className="min-w-sm border-muted bg-background flex w-full max-w-sm flex-col items-center gap-y-4 rounded-md border px-6 py-8 shadow-md">
-            {heading && <h1 className="text-xl font-semibold">{heading}</h1>}
-            <div className="flex w-full flex-col gap-2">
-              <Label>Nome</Label>
-              <Input
-                type="text"
-                name="name"
-                placeholder="Whisper"
-                className="text-sm"
-                required
-              />
+              {heading && <h1 className="text-xl font-semibold">{heading}</h1>}
+              <div className="flex w-full flex-col gap-2">
+                <Label>Nome</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="Whisper"
+                  className="text-sm"
+                  required
+                />
+              </div>
+              <div className="flex w-full flex-col gap-2">
+                <Label>Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="whisper@email.com"
+                  className="text-sm"
+                  required
+                />
+              </div>
+              <div className="flex w-full flex-col gap-2">
+                <Label>Senha</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="*******"
+                  className="text-sm"
+                  required
+                />
+              </div>
+              <div className="flex w-full flex-col gap-2">
+                <Label>Confirme sua senha</Label>
+                <Input
+                  type="password"
+                  name="confirm"
+                  placeholder="*******"
+                  className="text-sm"
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                {buttonText}
+              </Button>
             </div>
-            <div className="flex w-full flex-col gap-2">
-              <Label>Email</Label>
-              <Input
-                type="email"
-                name="email"
-                placeholder="whisper@email.com"
-                className="text-sm"
-                required
-              />
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <Label>Senha</Label>
-              <Input
-                type="password"
-                name="password"
-                placeholder="*******"
-                className="text-sm"
-                required
-              />
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <Label>Confirme sua senha</Label>
-              <Input
-                type="password"
-                name="confirm"
-                placeholder="*******"
-                className="text-sm"
-                required
-              />
-            </div>
-            <Button type="submit" className="w-full">
-              {buttonText}
-            </Button>
-          </div>
           </form>
-          
+
           <div className="text-muted-foreground flex justify-center gap-1 text-sm">
             <p>{signupText}</p>
             <a
